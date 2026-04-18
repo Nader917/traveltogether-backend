0bd9a5bb60b6db58f982486a755df081ab2be2ec
Nader917 <nader.20231899@gmail.com>
2026-04-18 02:27:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get("/", (req, res) => {
 });
 
 // Angular fallback
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/bus-traveller/index.html'));
 });
 
